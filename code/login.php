ac32e364e051fb4bcb5760994ccb5bce5c37fe69
VidKot <169042809+VidKot@users.noreply.github.com>
2024-09-17 18:17:56 +0200

    Update login.php

---
+++ code/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <form action="main.php" method="post">
+    <label for="textbox1">Textbox 1:</label>
+    <input type="text" id="textbox1" name="textbox1"><br><br>
+
+    <label for="textbox2">Textbox 2:</label>
+    <input type="text" id="textbox2" name="textbox2"><br><br>
+
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
