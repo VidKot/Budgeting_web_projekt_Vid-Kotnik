c493a4243eac9bf2dc23a5a6bf5b1c75f03f4a72
VidKot <169042809+VidKot@users.noreply.github.com>
2024-09-17 18:18:51 +0200

    Update register.php

---
+++ code/register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Textboxes and Submit Button</title>
+</head>
+<body>
+  <form action="main.php" method="post">
+    <label for="textbox1">Textbox 1:</label>
+    <input type="text" id="textbox1" name="textbox1"><br><br>
+
+    <label for="textbox2">Textbox 2:</label>
+    <input type="text" id="textbox2" name="textbox2"><br><br>
+
+<label for="textbox1">Textbox 1:</label>
+    <input type="text" id="textbox1" name="textbox1"><br><br>
+
+    <label for="textbox2">Textbox 2:</label>
+    <input type="text" id="textbox2" name="textbox2"><br><br>
+
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
