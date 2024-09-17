8069ff47fddc719486c81f2c7414df75670a250c
VidKot <169042809+VidKot@users.noreply.github.com>
2024-09-17 18:20:49 +0200

    Update code

---
+++ code/main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Wallet App</title>
+  <link rel="stylesheet" href="style.css"> </head>
+<body>
+  <header>
+    </header>
+
+  <main>
+    <div class="buttons">
+      <button>Top up</button>
+      <button>Received</button>
+      <button>Gift</button>
+    </div>
+
+    <section class="quick-transfer">
+      <h2>Quick Transfer</h2>
+      <form>
+        <label for="amount">Amount:</label>
+        <input type="number" id="amount" name="amount">
+        <select name="currency">
+          <option value="USD">USD</option>
+          </select>
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  </main>
+
+  <footer>
+    <h2>Subscribe Transactions</h2>
+    </footer>
+</body>
+</html>
