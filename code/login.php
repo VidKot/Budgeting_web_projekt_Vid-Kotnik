12d55ae22128d341dbc0820fd413ddc6e4034c77
VidKot <169042809+VidKot@users.noreply.github.com>
2024-09-18 09:31:16 +0200

    Update login.php

--- code/login.php
+++ code/login.php
@@ -1,17 +1,34 @@
+<?php
+$valid_username = 'admin';
+$valid_password = 'password123';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username === $valid_username && $password === $valid_password) {
+    echo "<h1>Login successful! Welcome, $username.</h1>";
+} else {
+    echo "<h1>Invalid username or password. Please try again.</h1>";
+    echo '<a href="main.php">Go back to login</a>';
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Login</title>
 </head>
 <body>
-  <form action="main.php" method="post">
-    <label for="textbox1">Textbox 1:</label>
-    <input type="text" id="textbox1" name="textbox1"><br><br>
-
-    <label for="textbox2">Textbox 2:</label>
-    <input type="text" id="textbox2" name="textbox2"><br><br>
-
-    <input type="submit" value="Submit">
-  </form>
+  <div class="login-container">
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        
+        <input type="submit" value="Login">
+    </form>
+  </div>
 </body>
 </html>
