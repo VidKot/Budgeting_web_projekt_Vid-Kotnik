91b1860b2aaa3832a0a2303c6115cfac404ad4c2
VidKot <169042809+VidKot@users.noreply.github.com>
2024-09-24 10:02:43 +0200

    .

--- code/index.php
+++ code/index.php
@@ -4,18 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="in.css"> 
 </head>
 <body>
     <div class="container">
         <h2>Welcome!</h2>
         
-        <form action="login.php" method="get">
-            <button type="submit">Login</button>
-        </form>
-        
-        <form action="register.php" method="get">
-            <button type="submit">Register</button>
-        </form>
+        <div class="button">
+            <form action="login.php" method="get">
+                <button type="submit">Login</button>
+            </form>
+            
+            <form action="register.php" method="get">
+                <button type="submit">Register</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
